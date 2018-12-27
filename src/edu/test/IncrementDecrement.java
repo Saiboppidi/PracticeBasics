c3af084354016ca38534b720aa6e7ed7efab39eb
sai <saikiranreddyboppidi@gmail.com>
2018-12-27 00:38:17 -0500

    increment decrement

---
+++ src/edu/test/IncrementDecrement.java
@@ -0,0 +1,47 @@
+package edu.test;
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =2;
+		/**
+		 * b=a;
+		 * a=a+1;
+		 */
+		int b = ++a  + a;
+		System.out.println(b);//b=2,a=3
+		/**
+		 * a=a+1;
+		 * b=a;
+		 */
+		b = ++a;
+		System.out.println(b);
+		int c = a;
+		System.out.println(b);
+		System.out.println(c);
+		b=3;
+		/**
+		 * a=b ;
+		 * b= b-1;
+		 */
+		a=b--; 
+		System.out.println(a);
+        a= 5;
+        /**
+         * a= a-1;
+         * b=a;
+         */
+        b= --a;      
+        System.out.println(b);
+        
+        a=12;
+        a = (a++)+(++a);
+        System.out.println("Guess ? "+ (a));
+        System.out.println(a);
+		
+	}
+	
+
+}
--- src/lesson13/Sai.java
+++ src/lesson13/Sai.java
@@ -4,12 +4,9 @@ public class Sai {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int[] myintarray = {1,2,3,4,5};
- {
-System.out.println(myintarray[3]);
+		int[] myintarray = { 1, 2, 3, 4, 5 };
+		{
+			System.out.println(myintarray[3]);
+		}
+	}
 }
-}
-
-	
-
-
